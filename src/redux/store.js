dd548cd414f8dec7cc9176058ec573504f4e4585
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-12-06 12:50:35 +0600

    redux-devtools-extension added to monitor store activities

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,10 @@
 import { createStore } from "redux";
 import { productReducer } from "./reducers/productReducer";
+import { composeWithDevTools } from "redux-devtools-extension"
 
-export const store = createStore(productReducer)
+const store = createStore(
+    productReducer, 
+    composeWithDevTools()
+)
+
+export default store
