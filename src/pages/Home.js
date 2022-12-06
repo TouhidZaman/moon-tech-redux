b87218802d7c2dbf02711b14fc4fac119e03f503
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-12-06 12:24:10 +0600

    basic react-redux setup added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const test = useSelector(state => state.test)
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+      <h1>{test}</h1>
     </div>
   );
 };
---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    test: "Redux setup successful"
+}
+
+export const productReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default: 
+            return state
+    }
+}
